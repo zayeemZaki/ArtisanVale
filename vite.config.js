b51865f9be4cc4b441a8951c2c1fa070f75253ce
zayeemZaki <zayeemzaki45@gmail.com>
2025-06-28 11:48:59 -0500

    fixed base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: 'ArtisanVale', // Replace with your GitHub repo name
+  base: '/ArtisanVale/',
   plugins: [vue()],
 });
