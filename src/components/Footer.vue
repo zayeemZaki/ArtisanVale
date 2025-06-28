f5fbd7a931bd8e0d1fd4adb349f45d33b31f694b
zayeemZaki <zayeemzaki45@gmail.com>
2025-06-28 12:26:55 -0500

    connect to domain

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,10 +2,10 @@
   <footer class="text-center text-sm py-4 text-gray-600 whitespace-nowrap overflow-x-auto px-4">
     <p>
       © 2025 <span class="text-pink-600 font-semibold">ArtisanVale</span> &nbsp;·&nbsp;
-      <a href="mailto:support@artisanvale.in" class="text-pink-500 hover:underline">
+      <a href="mailto:artisansvale@gmail.com" class="text-pink-500 hover:underline">
         support@artisanvale.in
       </a> &nbsp;·&nbsp;
-      📸 <a href="https://instagram.com/yourhandle" target="_blank" class="text-pink-500 hover:underline">
+      📸 <a href="https://www.instagram.com/artisanvale/" target="_blank" class="text-pink-500 hover:underline">
         Instagram
       </a>
     </p>
