d01e043d444f9898108b2860cc753a214f227bec
zayeemZaki <zayeemzaki45@gmail.com>
2025-06-28 13:47:50 -0500

    updated footer to two lines

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,20 @@
 <template>
-  <footer class="text-center text-sm py-4 text-gray-600 whitespace-nowrap overflow-x-auto px-4">
-    <p>
-      © 2025 <span class="text-pink-600 font-semibold">ArtisanVale</span> &nbsp;·&nbsp;
-      <a href="mailto:artisansvale@gmail.com" class="text-pink-500 hover:underline">
-        artisansvale@gmail.com
-      </a> &nbsp;·&nbsp;
-      📸 <a href="https://www.instagram.com/artisanvale/" target="_blank" class="text-pink-500 hover:underline">
-        Instagram
-      </a>
-    </p>
+  <footer class="text-center text-sm py-4 text-gray-600 px-4">
+    <div class="flex flex-col sm:flex-row sm:justify-center sm:items-center gap-1 sm:gap-2">
+      <div>
+        <a href="mailto:artisansvale@gmail.com" class="text-pink-500 hover:underline">
+          artisansvale@gmail.com
+        </a>
+        &nbsp;·&nbsp;
+        📸
+        <a href="https://www.instagram.com/artisanvale/" target="_blank" class="text-pink-500 hover:underline">
+          Instagram
+        </a>
+      </div>
+      <div class="sm:ml-2">
+        © 2025 <span class="text-pink-600 font-semibold">ArtisanVale</span>
+      </div>
+    </div>
   </footer>
 </template>
 
