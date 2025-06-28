91c3884fb6f4b4173ef2966ec91e1f7f31f7f13a
zayeemZaki <zayeemzaki45@gmail.com>
2025-06-28 14:07:29 -0500

    made it more rounded

--- src/components/ChatBubble.vue
+++ src/components/ChatBubble.vue
@@ -4,7 +4,7 @@
       sender === 'bot'
         ? 'bg-pink-100 text-pink-800 self-start'
         : 'bg-blue-100 text-blue-900 self-end',
-      'rounded-xl px-6 py-4 max-w-[95%] w-fit shadow-lg text-base sm:text-xl font-semibold leading-relaxed mt-4 mb-2 mx-auto text-center'
+      'rounded-3xl px-6 py-4 max-w-[95%] w-fit shadow-lg text-base sm:text-xl font-semibold leading-relaxed mt-4 mb-2 mx-auto text-center'
     ]"
   >
     <slot />
