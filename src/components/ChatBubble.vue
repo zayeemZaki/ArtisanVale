f1f30f31aae867ebf4a1108dde0a4b710308da94
zayeemZaki <zayeemzaki45@gmail.com>
2025-06-28 13:54:52 -0500

    updated chat bubble color

--- src/components/ChatBubble.vue
+++ src/components/ChatBubble.vue
@@ -2,9 +2,9 @@
   <div
     :class="[
       sender === 'bot'
-        ? 'bg-yellow-100 text-yellow-900 self-start'
+        ? 'bg-pink-100 text-pink-800 self-start'
         : 'bg-blue-100 text-blue-900 self-end',
-      'rounded-xl px-6 py-4 max-w-[95%] w-fit shadow-lg text-base sm:text-xl leading-relaxed mt-4 mb-2 mx-auto text-center'
+      'rounded-xl px-6 py-4 max-w-[95%] w-fit shadow-lg text-base sm:text-xl font-semibold leading-relaxed mt-4 mb-2 mx-auto text-center'
     ]"
   >
     <slot />
